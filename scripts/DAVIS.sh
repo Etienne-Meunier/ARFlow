c7cfd16a6e37fcfef328dbe6812656d244c9eb13
Etienne Meunier <etienne.meunier@inria.fr>
2024-01-03 15:56:35 +0100

    DAVIS

--- scripts/DAVIS.sh
+++ scripts/DAVIS.sh
@@ -2,5 +2,7 @@ for i in $Dataria/DAVIS/JPEGImages/480p/*;
   do
     trap <command> SIGINT
     echo $i
-    python3 batch_flow.py -i $i -od $Dataria/DAVIS/OpticalFlowARFlow/
+    python3 batch_flow.py -i $i -od $Dataria/DAVIS/OpticalFlowARFlowKitti1/\
+                          --model checkpoints/KITTI15/pwclite_ar.tar\
+                          -s 448 1024
   done
